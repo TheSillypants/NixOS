a056b4e56fe46d106cd88c51185fae9e24ac521f
TheSillypants <TheSillypants@example.com>
2026-09-26 12:32:10 -0400

    Add key to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,4 +1,8 @@
-let 
+let
+  master = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmOpaMT4tRogFiQzz8chVBA9ehl7DGcZe+k8OPKW/ec";
+  masterkey = [ master ];
+
+  
   luka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKUqE5Pl1DbgSGpzLS7ZpL5IrgTQ984sdhVUWOOB2Lfk";
   users = [ luka ];
 
@@ -6,9 +10,9 @@ let
   systems = [ hephaestus ];
 in
 {
-  "secret1.age".publicKeys = users ++ systems;
-  "githubssh.age".publicKeys = users ++ systems;
-  "githubssh_pub.age".publicKeys = users ++ systems;
+  "secret1.age".publicKeys = users ++ systems ++ masterkey;
+  "githubssh.age".publicKeys = users ++ systems ++ masterkey;
+  "githubssh_pub.age".publicKeys = users ++ systems ++ masterkey;
 
 
 }
